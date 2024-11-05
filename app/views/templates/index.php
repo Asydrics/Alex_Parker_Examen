1933d1ad844459ba2aa467b30ad75b1ab9899fd7
Asydrics <maximeleroy001@gmail.com>
2024-11-05 13:43:36 +0100

    Intégration des partials

--- app/views/templates/index.php
+++ app/views/templates/index.php
@@ -1,11 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Coucou</h1>
-</body>
+  <head>
+  <?php include "../app/views/templates/partials/_head.php"; ?>
+  </head>
+
+  <body>
+    <!-- Preloader Start -->
+    <div class="preloader">
+      <div class="rounder"></div>
+    </div>
+    <!-- Preloader End -->
+
+    <div id="main">
+      <div class="container">
+      <?php include "../app/views/templates/partials/_main.php"; ?>
+      </div>
+    </div>
+
+    <!-- Back to Top Start -->
+    <a href="#" class="scroll-to-top"><i class="fa fa-long-arrow-up"></i></a>
+    <!-- Back to Top End -->
+
+    <!-- All Javascript Plugins  -->
+    <?php include "../app/views/templates/partials/_script.php"; ?>
+  </body>
 </html>
